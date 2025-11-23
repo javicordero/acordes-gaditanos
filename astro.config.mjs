982d9f634f3d9131c589eeb664b7547992922852
Javi Cordero <javiercorderotoscano@gmail.com>
2025-11-23 02:45:29 +0100

    Integración de Google Analytics y Partytown; Añadido eventos de GA en los botones de donar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   integrations: [
     icon({
       iconDir: 'src/icons', // o la ruta donde tengas tus SVGs
     }),
+    partytown({ config: { forward: ['dataLayer.push'] } }),
   ],
 
   vite: {
