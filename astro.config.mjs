93ab7d2c950c4e92d2dbd4445a3868ef4057c344
Javi Cordero <javiercorderotoscano@gmail.com>
2025-11-24 20:43:25 +0100

    Add sitemap configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://acordesgaditanos.com',
   integrations: [
     icon({
       iconDir: 'src/icons', // o la ruta donde tengas tus SVGs
     }),
     partytown({ config: { forward: ['dataLayer.push'] } }),
+    sitemap({
+      filter: (page) => !page.includes('/coming-soon'),
+    }),
   ],
 
   vite: {
