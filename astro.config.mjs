fc5b96e450cb43f44a5e1403da49a25ab34201bc
Javi Cordero <javiercorderotoscano@gmail.com>
2025-11-26 23:08:28 +0100

    Eliminar dependencia de Partytown y añadir script de análisis de Cloudflare en el encabezado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
     icon({
       iconDir: 'src/icons', // o la ruta donde tengas tus SVGs
     }),
-    partytown({ config: { forward: ['dataLayer.push'] } }),
     sitemap({
       filter: (page) => !page.includes('/coming-soon'),
     }),
